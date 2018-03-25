cc312ec395fa8e68655851bead5306f2656708ff
hujuan <hujuanzp@163.com>
2018-03-25 19:25:12 +0800

    add a class

---
+++ src/test/Hujuan.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author kevingreenzp
+ *
+ */
+public class Hujuan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+	}
+
+}
